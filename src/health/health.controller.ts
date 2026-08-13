434b9287a139a9e6e67e2d1dc5a7a03cd00a7e68
Selamawitk <selamawitkinetibeb@gmail.com>
2026-08-13 18:14:43 +0300

    fix(health): check disk usage on app directory with higher threshold

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -24,7 +24,10 @@ export class HealthController {
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
       () => this.db.pingCheck('database'),
       () =>
-        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+        this.disk.checkStorage('storage', {
+          path: process.cwd(),
+          thresholdPercent: 0.9,
+        }),
       //   () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
       //   () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
     ]);
